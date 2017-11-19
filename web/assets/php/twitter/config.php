cbff7d2c88f044212fd197c97e5cd4706f44fd9b
Meg4R0M <psykoterro@gmail.com>
2017-11-19 21:34:51 +0100

    first commit

---
+++ web/assets/php/twitter/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+
+    // Consumer Key
+    define('CONSUMER_KEY', 'z1H8HhqkbnqVwn11o3R2YgUmW');
+    define('CONSUMER_SECRET', 'XSSPOz11Zza86wlIeaOTTX4cBsP7Y1jOgD6JC9L4HctrzA7g1a');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '121077275-MD9hCDPY1SXLeD4KhYDYggIEG5cdDdRKAvGwc9Rr');
+    define('ACCESS_SECRET', 'Ag84OMgJA3sjq7l64IgybFju0xfW4u3JswMBTaQgASGFU');
